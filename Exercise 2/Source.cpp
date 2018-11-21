419ee7347def547da0c24dbc7237b19a40b82e28
Олжас Биахметов <@local.itstep>
2018-11-21 21:02:01 +0600

    Добавьте файлы проекта.

---
+++ Exercise 2/Source.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <stdio.h>
+#include <locale.h>
+int main()
+{
+	
+	/*Task 1
+	int x, y;
+
+	printf("x = ");
+	scanf_s("%d", &x);
+
+	y = 3 * pow(x, 6) - 6 * pow(x, 2) - 7;
+	printf("y = %d\n", y);
+
+	system("pause");
+	*/
+	
+	/*Task 2
+	int x, y;
+
+	printf("x = ");
+	scanf_s("%d", &x);
+
+	y = 4 * pow(x-3, 6) - 7 * pow(x-3, 3) + 2;
+	printf("y = %d\n", y);
+
+	system("pause");
+	*/
+
+	/*Task 3
+	int a;
+
+	printf("a = ");
+	scanf_s("%d", &a);
+
+	a = a * a;
+	printf("a^2 = %d\n", a);
+
+	a = a * a;
+	printf("a^4 = %d\n", a);
+
+	a = a * a;
+	printf("a^8 = %d\n", a);
+
+	system("pause");
+	*/
+
+	/*Task 4
+	int a, b;
+
+	printf("a = ");
+	scanf_s("%d", &a);
+
+	b = a * a;
+	printf("a^2 = %d\n", b);
+
+	a = b * a;
+	printf("a^3 = %d\n", a);
+
+	a = a * b;
+	printf("a^5 = %d\n", a);
+
+	b = a * a;
+	printf("a^10 = %d\n", b);
+
+	a = a * b;
+	printf("a^15 = %d\n", a);
+
+	system("pause");
+	*/
+
+	/*Task 5
+	setlocale(LC_ALL, "");
+	int tf;
+	float tc;
+
+	printf("Температура в градусах Фаренгейта = ");
+	scanf_s("%d", &tf);
+
+	tc = (tf - 32) * 5 / 9;
+	printf("Температура в градусах Цельсия = %f\n", tc);
+
+	system("pause");
+	*/
+}
